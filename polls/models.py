82633d67ac1a8964d1afe079dd0e1c9912adb795
robert1074004 <abc50113@yahoo.com.tw>
2023-07-11 20:43:41 +0800

    fead: add article seeds

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from faker import Faker
+
+fake = Faker()
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     image = models.URLField()
-    content = models.TextField()
+    content = models.TextField(default = fake.paragraph)
     pub_date = models.DateTimeField(default=timezone.now)
