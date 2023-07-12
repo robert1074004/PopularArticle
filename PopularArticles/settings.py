e422cebc47218ad9f1254ce4ac4bfabf66b5c5be
robert1074004 <abc50113@yahoo.com.tw>
2023-07-12 14:57:07 +0800

    feat: add restframework

--- PopularArticles/settings.py
+++ PopularArticles/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
