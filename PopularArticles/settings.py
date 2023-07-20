59f2aceabc623e80534cdd699f629baa32b7c590
robert1074004 <abc50113@yahoo.com.tw>
2023-07-20 20:05:42 +0800

    feat: add new user_seed

--- PopularArticles/settings.py
+++ PopularArticles/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    # 'rest_framework_jwt'
+    "rest_framework_jwt",
+    'django_extensions'
 ]
 
 # REST_FRAMEWORK = {
